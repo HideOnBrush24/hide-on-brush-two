853f33b7d71002338b73da33ee4f112a05be23d1
Nicholas Spoletini <nicholass24@109d25pf0fdf8j7.local>
2019-02-04 14:12:47 -0700

    Initial Commit

---
+++ hide on brush two/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hide on brush two
+//
+//  Created by Nicholas Spoletini on 2019-02-04.
+//  Copyright © 2019 Nicholas Spoletini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
